9c1f60cd60851b451971bfc753d68a2efb3ac262
Oleksandr-N <hekp1984@gmail.com>
2021-11-19 12:51:54 +0200

    дз 9 V2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(element) {
+  element.preventDefault();
+  const { delay, step, amount } = element.currentTarget;
+
+  let delayEl = Number(delay.value);
+  const stapEl = Number(step.value);
+  const amounEl = Number(amount.value);
+  for (let position = 1; position <= amounEl; position += 1) {
+    createPromise(position, delayEl);
+    console.log('delayPromise', delayEl, 'position', position);
+    delayEl += stapEl;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  promise
+    .then(({ position, delay }) => {
+      Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
